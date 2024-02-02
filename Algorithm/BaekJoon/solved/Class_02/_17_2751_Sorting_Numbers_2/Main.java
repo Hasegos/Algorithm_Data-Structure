cde7698ea8fe1b8d79f79634e0bff55e92debd80
Hasegos <csw020106@naver.com>
2024-02-02 01:02:44 +0000

    백준 알고리즘 수 정렬하기 2

--- Algorithm/BaekJoon/solved/Class_02/_17_2751_Sorting_Numbers_2/Main.java
+++ Algorithm/BaekJoon/solved/Class_02/_17_2751_Sorting_Numbers_2/Main.java
@@ -4,7 +4,19 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
-// https://yuja-kong.tistory.com/183 이거 보고 이해하기 여기서부터 시작하기
+/*
+    https://www.acmicpc.net/problem/2751
+    
+    문제 : N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+    
+    입력 : 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다.
+          이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+    
+    출력 : 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+    
+    sort()를 활용해서 단어 정렬을 시키는데 중요한건 '시간 복잡도' 이다.
+    시간 복잡도 관련해서 Arrays.sort() 와 Collections.sort() 차이를 알아야한다.
+*/
 public class Main {
     
     // Arrays.sort()로 정렬하는 방법 대신 O(n^2)으로 시간 초과가 날수있다.
