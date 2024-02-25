455596f5a5d862cff4708d4073b27bee5ea4c5e5
Hasegos <csw020106@naver.com>
2024-02-25 06:06:44 +0000

    Java Book God Chapter 2 Basic Structure END

---
+++ Java Basic/Java_Book_God/Java_VOL.1/Java_Chapter02/HelloGodOfJava.java
@@ -0,0 +1,14 @@
+
+public class HelloGodOfJava {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello God of Java!!!");
+        System.out.println("I love this book");        
+        
+        System.out.print("Hello God of Java!!!");
+        System.out.print("I love this book");
+        
+        
+    }
+}
