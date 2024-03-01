36cb72d8cdbafff01e1143bab23ee4e3f70933f8
최수호 <93961708+Hasegos@users.noreply.github.com>
2024-03-01 04:14:20 +0000

    모두를 위한 컴퓨터 과학(CS50 2019) - 배열 4) 배열(1)

---
+++ Computer Science/What Computer Science/Arrays/Array(1)/hi.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+    // casting(형 변환) : 대상 변수를 원하는 형태로 변환해줌
+    printf("%i %i %i\n", c1, c2, c3);
+}
