a4ba5644c1e7cc749e31d481a065faa037fa3fb0
최수호 <93961708+Hasegos@users.noreply.github.com>
2024-03-01 02:11:08 +0000

    모두를 위한 컴퓨터 과학(CS50 2019) - 배열 3) 코드의 디자인

---
+++ Computer Science/What Computer Science/Arrays/Design Of Code/hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// 여러가지 형태로 코드를 짤 수 있다. 하지만 보기좋고 이해하기쉽게 짜야한다.
+// case 1 
+int main(void){
+    printf("hello, world\n");
+}
+// case 2
+int main(void){printf("hello, world\n");}
+// case 3
+int main(void)
+    {
+printf("hello, world\n");
+    }
