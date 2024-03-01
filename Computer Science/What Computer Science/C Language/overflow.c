1ebf924bf400e6be5b29a7e6be6c0f620910df5b
최수호 <93961708+Hasegos@users.noreply.github.com>
2024-03-01 00:46:07 +0000

    모두를 위한 컴퓨터 과학(CS50 2019) - C 하드웨어의 한계

--- Computer Science/What Computer Science/C Language/overflow.c
+++ Computer Science/What Computer Science/C Language/overflow.c
@@ -2,7 +2,7 @@
 #include<unistd.h>
 
 int main(void){
-    // 정수 오버플로일어나면서 앞으로 넘어갈 1의자리가 사라진다.
+    // 정수 오버플로우일어나면서 앞으로 넘어갈 1의자리가 사라진다.
     for(int i = 1; i *=2;){        
         printf("%i\n",i);
         sleep(1);
